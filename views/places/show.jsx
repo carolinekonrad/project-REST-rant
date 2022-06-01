2485edff5a3977f8dbc9d2e9d7d4f6a5cbb7246e
Caroline Konrad <ckonrad@live.unc.edu>
2022-05-31 20:24:32 -0400

    Adding average star rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,20 @@ function show (data) {
         <h3 className='inactive'>No comments yet!</h3>
     )
 
+    let rating = (
+        <h3 className="inactive">Not yet rated</h3>
+    )
+    if (data.place.comments.length){
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
+    }
     if (data.place.comments.length) {
         comments = data.place.comments.map(c => {
             return (
@@ -35,11 +49,9 @@ function show (data) {
                     { data.place.name }
                 </h1>
                 <h2>
-                    Rating
+                    {rating}
                 </h2>
-                <h3 className="inactive">
-                    Not Rated
-                </h3>
+                
             <br />
                 <h2>
                     Description
