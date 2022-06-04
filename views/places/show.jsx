da2ec2243ef6d6a93442c1918dff308b5ff204e7
Caroline Konrad <ckonrad@live.unc.edu>
2022-06-03 21:18:35 -0400

    Add emojis to comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -28,7 +28,7 @@ function show (data) {
         comments = data.place.comments.map(c => {
             return (
                 <div className='border'>
-                    <h2>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h2>{c.rant ? 'Rant!ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
                     <h4>{c.content}</h4>
                     <h3>
                         <strong>- {c.author}</strong>
